eba6c73be79bd9dc8758eebb10336354a87bbf99
matthew-intersect <matthew@intersect.org.au>
2013-08-18 12:05:20 +1000

    remove menu from home screen

--- src/com/death/artovmetal/HomeActivity.java
+++ src/com/death/artovmetal/HomeActivity.java
@@ -3,7 +3,6 @@ package com.death.artovmetal;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,14 +33,6 @@ public class HomeActivity extends Activity
 				startActivity(imageBoard);
 			}
 		});
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
 	}
 
 }
